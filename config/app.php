81f1e178f584138a4f62c670ec46b6f120487864
greyhats13 <imamariefrahmann@gmail.com>
2019-03-29 14:36:40 +0700

    perubahan config

--- config/app.php
+++ config/app.php
@@ -52,7 +52,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'http://arenahormonindonusa.xyz/crm'),
+    'url' => env('APP_URL', 'http://arenahormonindonusa.xyz/crm/'),
 
     'asset_url' => env('ASSET_URL', null),
 
